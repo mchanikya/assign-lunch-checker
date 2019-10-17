36beb2ad0b78b08caa4588b286defea39ea16cbc
Chanikya <chanikyaprakash@gmail.com>
2019-10-17 20:28:33 +0530

    removed

--- app.js
+++ app.js
@@ -5,9 +5,9 @@
 
 // .controller('HungerAdvisorController',HungerAdvisorController);
 
-// HungerAdvisorController.$inject = ['$scope','$filter'];
+// HungerAdvisorController.$inject = ['$scope'];
 
-//  function HungerAdvisorController($scope,$filter) {
+//  function HungerAdvisorController($scope) {
 //   $scope.message = "";
 //   $scope.foodItems = "";
 //   $scope.calculateFoodItems = function () {
@@ -26,5 +26,6 @@
 // }
 // })();
 
+
 (function(){'use strict'
-angular.module('HungerAdvisor',[]).controller('HungerAdvisorController',HungerAdvisorController);HungerAdvisorController.$inject=['$scope','$filter'];function HungerAdvisorController($scope,$filter){$scope.message="";$scope.foodItems="";$scope.calculateFoodItems=function(){var totalFoodItems="noitems";var fItems=$scope.foodItems.split(',');if(fItems.length>=1&&fItems.length<=3){if($scope.foodItems.length==0){$scope.message='"Please enter data first"'}else{$scope.message='"Enjoy!"'}}else if(fItems.length>3){$scope.message='"Too much!"'}}}})()
+angular.module('HungerAdvisor',[]).controller('HungerAdvisorController',HungerAdvisorController);HungerAdvisorController.$inject=['$scope'];function HungerAdvisorController($scope){$scope.message="";$scope.foodItems="";$scope.calculateFoodItems=function(){var totalFoodItems="noitems";var fItems=$scope.foodItems.split(',');if(fItems.length>=1&&fItems.length<=3){if($scope.foodItems.length==0){$scope.message='"Please enter data first"'}else{$scope.message='"Enjoy!"'}}else if(fItems.length>3){$scope.message='"Too much!"'}}}})()
